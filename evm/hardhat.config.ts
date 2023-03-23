2c4c4ff4376bec8716da18f12a014754f5c442a9
Nick <nick@enshrine.ai>
2023-03-23 01:20:04 +0000

    evm: deploy to hyperspace

--- evm/hardhat.config.ts
+++ evm/hardhat.config.ts
@@ -1,48 +1,67 @@
-import { HardhatUserConfig } from 'hardhat/config';
-import '@nomicfoundation/hardhat-toolbox';
-import 'hardhat-watcher';
-import 'hardhat-deploy';
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-watcher";
+import "hardhat-deploy";
 
-const accounts = process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [];
+const accounts = process.env.PRIVATE_KEY
+  ? [process.env.PRIVATE_KEY]
+  : process.env.MNEMONIC
+  ? { mnemonic: process.env.MNEMONIC }
+  : [];
+
+task('accounts')
+.setAction(async (_, hre) => {
+  const signers = await hre.ethers.getSigners();
+  const balances = await Promise.all(signers.map(s => hre.ethers.provider.getBalance(s.address)));
+  for (let i = 0; i < signers.length; i++) {
+    let num: string | number;
+    try {
+      num = balances[i].toNumber();
+    } catch {
+      num = balances[i].toString();
+    }
+    console.log(signers[i].address, num);
+  }
+});
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: '0.8.18',
+    version: "0.8.18",
     settings: {
       optimizer: {
-        enabled: true
+        enabled: true,
       },
-      viaIR: true
-    }
+      viaIR: true,
+    },
   },
   networks: {
     local: {
-      url: 'http://127.0.0.1:8545'
+      url: "http://127.0.0.1:8545",
     },
-    'sapphire-testnet': {
-      url: 'https://testnet.sapphire.oasis.dev',
+    "sapphire-testnet": {
+      url: "https://testnet.sapphire.oasis.dev",
       chainId: 0x5aff,
       accounts,
     },
     sapphire: {
-      url: 'https://sapphire.oasis.io',
+      url: "https://sapphire.oasis.io",
       chainId: 0x5afe,
       accounts,
     },
     hyperspace: {
-      url: 'https://rpc.ankr.com/filecoin_testnet',
+      url: "https://rpc.ankr.com/filecoin_testnet",
       chainId: 3141,
       accounts,
     },
   },
   watcher: {
     compile: {
-      tasks: ['compile'],
+      tasks: ["compile"],
     },
   },
   namedAccounts: {
-    deployer: 0
-  }
+    deployer: 0,
+  },
 };
 
 export default config;
