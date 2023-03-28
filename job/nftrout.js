2df74b03169d740158b58f29aa692498a6f31b9d
Nick <nick@enshrine.ai>
2023-03-28 21:05:38 +0000

    job: encrypt trout seeds

--- job/nftrout.js
+++ job/nftrout.js
@@ -1,39 +1,99 @@
-const { randomInt } = require('crypto');
-const fs = require('fs');
+const { randomBytes, randomInt } = require("crypto");
+const fs = require("fs");
 
-const { main: fishdraw, draw_svg: toSvg } = require('./fishdraw');
+const deoxysii = require("deoxysii");
 
+const { main: fishdraw, draw_svg: toSvg } = require("./fishdraw");
+
+const ENCODING = "base64url";
 const MAX_SEED = 4294967295; // 2^32-1
+
+const LATEST_KEY_ID = 0;
+
+const encode = (b) => Buffer.from(b).toString(ENCODING);
+const decode = (s) => Buffer.from(s, ENCODING);
+
+const getCipher = memoizeAsync(async (keyId) => {
+  let key;
+  if (keyId === 0) key = Buffer.alloc(deoxysii.KeySize, 42);
+  else throw new Error(`unknown key: ${keyId}`);
+  return new deoxysii.AEAD(key);
+});
 
 let [_node, _thisfile, tokenId, inputsDir, outputsDir] = process.argv;
 
-inputsDir = inputsDir ?? '/inputs';
-outputsDir = outputsDir ?? '/outputs';
+if (tokenId === undefined) throw new Error("missing tokenId");
 
-fs.access(`${inputsDir}/left.json`, (err) => {
+inputsDir = inputsDir ?? "/inputs";
+outputsDir = outputsDir ?? "/outputs";
+
+fs.access(`${inputsDir}/left/trout.json`, async (err) => {
   if (err) {
     nftrout(null, null, randomInt(0, MAX_SEED), tokenId);
     return;
   }
-  const left = readMetadata(`${inputsDir}/left/trout.json`);
-  const right = readMetadata(`${inputsDir}/right/trout.json`);
-  let seed = BigInt(left.seed) * 3n + BigInt(right.seed) * 5n + BigInt(randomInt(0, 256));
+  const left = await readMetadata(`${inputsDir}/left/trout.json`);
+  const right = await readMetadata(`${inputsDir}/right/trout.json`);
+  let seed =
+    BigInt(left.seed) * 3n + BigInt(right.seed) * 5n + BigInt(randomInt(0, 64));
   seed = Number(seed % BigInt(MAX_SEED));
-  nftrout(left.id, right.id, seed, tokenId);
+  await nftrout(left.tokenId, right.tokenId, seed, tokenId);
 });
 
-function readMetadata(fp) {
-  return JSON.parse(fs.readFileSync(fp));
+async function readMetadata(fp) {
+  const troutMeta = JSON.parse(fs.readFileSync(fp));
+  if (typeof troutMeta.seed === "number") return troutMeta;
+  const { keyId, nonce, data } = troutMeta.seed;
+  const cipher = await getCipher(keyId);
+  const plaintext = Buffer.from(cipher.decrypt(decode(nonce), decode(data)));
+  const { seed } = JSON.parse(plaintext);
+  troutMeta.seed = seed;
+  return troutMeta;
 }
 
-function nftrout(leftId, rightId, seed, tokenId) {
+async function nftrout(leftId, rightId, seed, tokenId) {
   const fishSvg = toSvg(fishdraw(seed));
 
   fs.writeFileSync(`${outputsDir}/trout.svg`, fishSvg);
-  fs.writeFileSync(`${outputsDir}/trout.json`, JSON.stringify({
-    left: leftId,
-    right: rightId,
-    tokenId,
-    seed, // TODO: encrypt
-  }));
+
+  const keyId = LATEST_KEY_ID;
+  const cipher = await getCipher(keyId);
+  const nonce = randomBytes(deoxysii.NonceSize);
+
+  fs.writeFileSync(
+    `${outputsDir}/trout.json`,
+    JSON.stringify({
+      left: leftId,
+      right: rightId,
+      tokenId,
+      seed: {
+        keyId,
+        nonce: encode(nonce),
+        data: encode(
+          cipher.encrypt(nonce, Buffer.from(JSON.stringify({ seed })))
+        ),
+      },
+    })
+  );
+}
+
+function memoizeAsync(fn) {
+  const cache = new Map();
+  const inProgress = new Map();
+  return async function memoized(...args) {
+    const key = JSON.stringify(args);
+    if (cache.has(key)) return cache.get(key);
+    if (inProgress.has(key)) return await inProgress.get(key);
+    const promise = fn(...args);
+    inProgress.set(key, promise);
+    try {
+      const result = await promise;
+      cache.set(key, result);
+      return result;
+    } catch (err) {
+      throw err;
+    } finally {
+      inProgress.delete(key);
+    }
+  };
 }
