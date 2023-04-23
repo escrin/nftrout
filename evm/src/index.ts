b1d403da563446c25368ea27a91d4689d6975d2f
Nick <nick@enshrine.ai>
2023-04-23 05:00:40 +0000

    evm: deploy to sapphire-testnet

--- evm/src/index.ts
+++ evm/src/index.ts
@@ -1,2 +1,11 @@
 export { NFTrout__factory as NFTroutFactory } from '../typechain-types/factories/contracts/NFTrout__factory';
 export { NFTrout } from '../typechain-types/contracts/NFTrout';
+
+import sapphireTestnet from '../deployments/sapphire-testnet/NFTrout.json';
+
+export const DEPLOYMENTS = {
+  0x5aff: sapphireTestnet,
+  0x5afe: null,
+  314: null,
+  3141: null,
+};
